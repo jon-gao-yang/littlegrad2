8c123a71b307f57e9422ec9c870dc133d59254f1
jon <jon.gao.yang@gmail.com>
2024-06-24 21:38:18 -0400

    implemented autodiff backprop for basic tensor methods

--- littlegrad2/engine.py
+++ littlegrad2/engine.py
@@ -2,37 +2,109 @@ import numpy as np
 
 class Tensor:
     def __init__(self, data = [], children = (), grad = 0, op = ''):
-        self.data = np.array(object = data)
+        self.data = np.array(object = data, dtype = float)
         self.children = children
         self.grad = grad
-        self.op = op
+        self.backward = lambda: None
+        self._op = op
+
     def __repr__(self):
         return f"Tensor object with data {self.data}"
     
     def __add__(self, other):
-        if type(other) != Tensor:
-            other = Tensor(other)
-        out = Tensor(self.data + other.data)
-        out.children = (self, other)
+        other = other if type(other) == Tensor else Tensor(other)
+        out = Tensor(data = (self.data + other.data), children = (self, other), op = '+')
+
+        def backward():
+            self.grad += out.grad
+            other.grad += out.grad
+        out.backward = backward
         return out
     
     def __sub__(self, other):
-        if type(other) != Tensor:
-            other = Tensor(other)
-        out = Tensor(self.data - other.data)
-        out.children = (self, other)
+        other = other if type(other) == Tensor else Tensor(other)
+        out = Tensor(data = (self.data - other.data), children = (self, other), op = '-')
+        
+        def backward():
+            self.grad += out.grad
+            other.grad += -out.grad
+        out.backward = backward
         return out
     
     def __mul__(self, other):
-        if type(other) != Tensor:
-            other = Tensor(other)
-        out = Tensor(self.data * other.data)
-        out.children = (self, other)
+        other = other if type(other) == Tensor else Tensor(other)
+        out = Tensor(data = (self.data * other.data), children = (self, other), op = '*')
+        
+        def backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+        out.backward = backward
         return out
     
     def __truediv__(self, other):
-        if type(other) != Tensor:
-            other = Tensor(other)
-        out = Tensor(self.data / other.data)
-        out.children = (self, other)
+        other = other if type(other) == Tensor else Tensor(other)
+        out = Tensor(data = (self.data / other.data), children = (self, other), op = '/')
+    
+        def backward():
+            self.grad += (1 / other.data) * out.grad
+            other.grad += (-self.data/(other.data**2)) * out.grad
+        out.backward = backward
         return out
+    
+    def __pow__(self, other):
+        other = other if type(other) == Tensor else Tensor(other)
+        out = Tensor(data = (self.data ** other.data), children = (self, other), op = '**')
+        
+        def backward():
+            self.grad += (other.data * (self.data ** (other.data - 1))) * out.grad
+            other.grad += (self.data ** other.data) * np.log(abs(self.data)+1e-10) * out.grad
+        out.backward = backward
+        return out
+    
+    def __radd__(self, other):
+        other = other if type(other) == Tensor else Tensor(other)
+        return other + self
+    
+    def __rsub__(self, other):
+        other = other if type(other) == Tensor else Tensor(other)
+        return other - self
+    
+    def __rmul__(self, other):
+        other = other if type(other) == Tensor else Tensor(other)
+        return other * self
+    
+    def __rtruediv__(self, other):
+        other = other if type(other) == Tensor else Tensor(other)
+        return other / self
+    
+    def __rpow__(self, other):
+        other = other if type(other) == Tensor else Tensor(other)
+        return other ** self
+    
+    def __neg__(self):
+        return self * -1
+    
+    def relu(self):
+        out = Tensor(data = max(0, self.data), children = (self,), op = 'ReLU')
+
+        def backward():
+            #self.grad += out.grad if self.data else 0 #negative numbers still evaluate to True for some dumb reason
+            self.grad += (self.data > 0) * out.grad
+        out.backward = backward
+        return out
+    
+    def backprop(self):
+        nodeList = []
+        visited = set()
+        def toposort(node):
+            for child in node.children:
+                if child not in visited:
+                    toposort(child)
+            visited.add(node)
+            nodeList.append(node)
+        toposort(self)
+
+        self.grad = 1
+        for node in reversed(nodeList):
+            node.backward()
+            #print(node, ': ', [child.grad for child in node.children]) #for debug
--- test.py
+++ test.py
@@ -1,12 +1,107 @@
+import torch
 from littlegrad2.engine import Tensor
 
-a = Tensor([2, 3])
-b = Tensor([5, 10])
+def my_test():
+    #a = Tensor([2, 3])
+    #b = Tensor([5, 10])
+    #c = 4
 
-print(a)
-print(b)
-print(a+b)
-print(a-b)
-print(a*b)
-print(a/b)
-print('test')
+    a = -5
+    b = 3
+    c = 1
+
+    aVal = Tensor(a)
+    bVal = Tensor(b)
+    print("a:      | Passed == ", type(aVal) == Tensor)
+    print("b:      | Passed == ", type(bVal) == Tensor)
+    print("a.data: | Passed == ", aVal.data == a)
+    print("b.data: | Passed == ", bVal.data == b)
+    print("a+c:    | Passed == ", (aVal+c).data == a+c)
+    print("c+a:    | Passed == ", (c+aVal).data == c+a)
+    print("a-c:    | Passed == ", (aVal-c).data == a-c)
+    print("c-a:    | Passed == ", (c-aVal).data == c-a)
+    print("a*c:    | Passed == ", (aVal*c).data == a*c)
+    print("c*a:    | Passed == ", (c*aVal).data == c*a)
+    print("-a:     | Passed == ", (-aVal).data == -a)
+    print("a/c:    | Passed == ", (aVal/c).data == a/c)
+    print("c/a:    | Passed == ", (c/aVal).data == c/a)
+    print("a**c:   | Passed == ", (aVal**c).data == a**c)
+    print("c**a:   | Passed == ", (c**aVal).data == c**a)
+    print()
+
+# from karpathy/micrograd test_engine.py:
+def test_sanity_check():
+
+    x = Tensor(-4.0)
+    z = 2 * x + 2 + x
+    q = z.relu() + z * x
+    h = (z * z).relu()
+    y = h + q + q * x
+    y.backprop()
+    xmg, ymg = x, y
+
+    x = torch.Tensor([-4.0]).double()
+    x.requires_grad = True
+    z = 2 * x + 2 + x
+    q = z.relu() + z * x
+    h = (z * z).relu()
+    y = h + q + q * x
+    y.backward()
+    xpt, ypt = x, y
+
+    # forward pass went well
+    assert ymg.data == ypt.data.item()
+    # backward pass went well
+    #print('xmg.grad: ', xmg.grad)
+    #print('xpt.grad.item(): ', xpt.grad.item())
+    assert xmg.grad == xpt.grad.item()
+    print('Karpathy #1: Passed == True')
+
+def test_more_ops():
+
+    a = Tensor(-4.0)
+    b = Tensor(2.0)
+    c = a + b
+    d = a * b + b**3
+    c += c + 1
+    c += 1 + c + (-a)
+    d += d * 2 + (b + a).relu()
+    d += 3 * d + (b - a).relu()
+    e = c - d
+    f = e**2
+    g = f / 2.0
+    g += 10.0 / f
+    g.backprop()
+    amg, bmg, gmg = a, b, g
+
+    a = torch.Tensor([-4.0]).double()
+    b = torch.Tensor([2.0]).double()
+    a.requires_grad = True
+    b.requires_grad = True
+    c = a + b
+    d = a * b + b**3
+    c = c + c + 1
+    c = c + 1 + c + (-a)
+    d = d + d * 2 + (b + a).relu()
+    d = d + 3 * d + (b - a).relu()
+    e = c - d
+    f = e**2
+    g = f / 2.0
+    g = g + 10.0 / f
+    g.backward()
+    apt, bpt, gpt = a, b, g
+
+    tol = 1e-6
+    # forward pass went well
+    assert abs(gmg.data - gpt.data.item()) < tol
+    # backward pass went well
+    assert abs(amg.grad - apt.grad.item()) < tol
+    assert abs(bmg.grad - bpt.grad.item()) < tol
+    print('Karpathy #2: Passed == True')
+    print()
+
+#############################################################################################
+
+my_test()
+test_sanity_check()
+test_more_ops()
