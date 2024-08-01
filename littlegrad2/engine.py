fixed backprop for Tensor.dftNd() and Tensor.idftNd()

littlegrad2/engine.py
@@ -115,16 +115,19 @@ class Tensor:
     def conv(self, other, stride = 1): # TODO: enforce correct matrix dims?
         other = other if type(other) == Tensor else Tensor(other)
         (nc, nx, ny) = self.data.shape # NOTE: CHANNELS FIRST
+        # ^ TODO: ADD M TO ALLOW VECTORIZATION (AND TRY TO USE SPLIT/CONCAT TO GET RID OF FOR LOOP)
         (fn, fc, fx, fy) = other.data.shape # NOTE: FILTERS & CHANNELS FIRST
-        out = Tensor(data = np.zeros(shape = (fn, ((nx-fx)//stride)+1, ((ny-fy)//stride)+1)), children = (self, other), op = 'conv')
+        out = Tensor(data = np.zeros(shape = (fn, ((nx-fx)//stride)+1, ((ny-fy)//stride)+1)), op = 'conv') # NOTE: doesn't need children/backwards because it's basically a literal
+        
         for f in range(fn):
             otherPadded = Tensor(data = np.zeros_like(self.data)).sliceAdd(other.slice((f)).flip(), (slice(fc), slice(fx), slice(fy)))
             out = out.sliceAdd((self.dftNd() * otherPadded.dftNd()).idftNd().slice((slice(0, 1, stride), slice(fx-1, nx, stride), slice(fy-1, ny, stride))), (slice(f, f+1), slice(out.data.shape[-2]), slice(out.data.shape[-1])))
+
         return out
     
     def avgPool(self, filter_size = 2, stride = 2):
         (nc, nx, ny) = self.data.shape
-        out = Tensor(data = np.zeros(shape = (nc, nx//stride, ny//stride)), children = (self,), op = 'avgPool')
+        out = Tensor(data = np.zeros(shape = (nc, nx//stride, ny//stride)), op = 'avgPool') # NOTE: doesn't need children/backwards because it's basically a literal
         filter = np.ones(shape = (1, 1, filter_size, filter_size))/(filter_size**2)
         for c in range(nc):
             out = out.sliceAdd(self.slice((slice(c, c+1))).conv(filter, stride = stride), (slice(c, c+1)))
@@ -132,7 +135,7 @@ class Tensor:
 
     def maxPool2d(self, filter_size = 2, stride = 2): # NOTE: FORWARD PASS NO WORK
         (nc, nx, ny) = self.data.shape
-        out = Tensor(data = np.ndarray(shape = (nc, nx//stride, ny//stride)), children = (self,), op = 'maxPool')
+        out = Tensor(data = np.ndarray(shape = (nc, nx//stride, ny//stride)), op = 'maxPool') # NOTE: doesn't need children/backwards because it's basically a literal
         for c in range(out.data.shape[0]):
             for x in range(out.data.shape[1]):
                 for y in range(out.data.shape[2]):
@@ -191,7 +194,7 @@ class Tensor:
         return out
     
     def dftNd(self):
-        out = Tensor(data = self.data, children = (self,), op = 'dft')
+        out = self
         dims = np.arange(len(self.data.shape))
         for dim in dims:
             shape = out.data.shape
@@ -200,7 +203,7 @@ class Tensor:
         return out
     
     def idftNd(self):
-        out = Tensor(data = self.data, children = (self,), op = 'idft')
+        out = self
         dims = np.arange(len(self.data.shape))
         for dim in dims:
             shape = out.data.shape

mnist.py
@@ -14,8 +14,8 @@ class TestNet:
             'f1' : Tensor(np.random.randn(6, 1, 5, 5) * np.sqrt(2 / (28*28*1))),
             'f2' : Tensor(np.random.randn(16, 6, 5, 5) * np.sqrt(2 / (12*12*6))),
 
-            #'w1' : Tensor(np.random.randn(24*24*6, 64) * np.sqrt(2 / (24*24*6))),
-            'w1' : Tensor(np.random.randn(12*12*6, 64) * np.sqrt(2 / (12*12*6))),
+            'w1' : Tensor(np.random.randn(24*24*6, 64) * np.sqrt(2 / (24*24*6))),
+            #'w1' : Tensor(np.random.randn(12*12*6, 64) * np.sqrt(2 / (12*12*6))),
             #'w1' : Tensor(np.random.randn(4*4*16, 64) * np.sqrt(2 / (4*4*16))),
             'b1' : Tensor(np.zeros((1, 64))),
             'w2' : Tensor(np.random.randn(64, 10) * np.sqrt(2 / (64))),
@@ -33,8 +33,8 @@ class TestNet:
             param.grad.fill(0)
 
     def __call__(self, x:Tensor) -> Tensor:
-        l2 = x.reshape((-1, 28, 28)).conv(self.params['f1']).relu().avgPool()
-        #l1 = x.reshape((1, 28, 28)).conv(self.params['f1']).relu().avgPool()
+        l2 = x.reshape((-1, 28, 28)).conv(self.params['f1']).relu()
+        #l1 = x.reshape((-1, 28, 28)).conv(self.params['f1']).relu().avgPool()
         #l2 = l1.conv(self.params['f2']).relu().avgPool()
         l3 = ((l2.flatten() @ self.params['w1']) + self.params['b1']).relu()
         return((l3 @ self.params['w2']) + self.params['b2'])
@@ -196,12 +196,13 @@ def kaggle_training(epochs = 10, batch_size = None, regularization = True, learn
         
         # update parameters w/ AdamW Algorithm
         for p in model.parameters(): 
-            p.data -= p.data * learning_rate * weight_decay
-            p.v = (beta1 * p.v) + ((1-beta1) * p.grad)
-            p.s = (beta2 * p.s) + ((1-beta2) * p.grad * p.grad)
-            v_dp_corrected = p.v / (1 - (beta1**(k+1)))
-            s_dp_corrected = p.s / (1 - (beta2**(k+1)))
-            p.data -= learning_rate * v_dp_corrected / (np.sqrt(s_dp_corrected) + epsilon) #doesn't work for broadasted bias v/s/grad tensors
+            p.data -= learning_rate * p.grad
+            # p.data -= p.data * learning_rate * weight_decay
+            # p.v = (beta1 * p.v) + ((1-beta1) * p.grad)
+            # p.s = (beta2 * p.s) + ((1-beta2) * p.grad * p.grad)
+            # v_dp_corrected = p.v / (1 - (beta1**(k+1)))
+            # s_dp_corrected = p.s / (1 - (beta2**(k+1)))
+            # p.data -= learning_rate * v_dp_corrected / (np.sqrt(s_dp_corrected) + epsilon) #doesn't work for broadasted bias v/s/grad tensors
 
         print(f"step {k} loss {total_loss.data.real[0, 0]}, accuracy {acc*100}%")
 
@@ -214,7 +215,7 @@ def kaggle_training(epochs = 10, batch_size = None, regularization = True, learn
 
 ###### [ 4/4 : MAIN FUNCTION EXECUTION ] ###### (NOTE: cost will not converge if learning rate is too high)
 
-kaggle_training(epochs = 20, batch_size = 10, regularization = False, learning_rate = 0.0001, alpha = 0)
+kaggle_training(epochs = 50, batch_size = 1, regularization = False, learning_rate = 0.00001, alpha = 0)
 #kaggle_training(epochs = 100, batch_size = 100, regularization = True, learning_rate = 0.0006, alpha = 1e-6)
 
 # for LinearNet():